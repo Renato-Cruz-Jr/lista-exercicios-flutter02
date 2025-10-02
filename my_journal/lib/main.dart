d7a9ae7c47d1dbf5d696a5889f1980678b4b128c
Renato Cruz Jr <renatofcruzjr@gmail.com>
2025-10-02 13:42:36 -0300

    feat: add my_journal Flutter app

---
+++ my_journal/lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'ui/screens/journal_screen.dart'; // 1. Importe sua tela de eventos
+
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Registro de Eventos',
+      debugShowCheckedModeBanner: false,
+
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.indigo),
+        useMaterial3: true,
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.indigo,
+          foregroundColor: Colors.white,
+        ),
+        floatingActionButtonTheme: const FloatingActionButtonThemeData(
+          backgroundColor: Colors.indigo,
+          foregroundColor: Colors.white,
+        ),
+      ),
+
+      home: const JournalScreen(),
+    );
+  }
+}
